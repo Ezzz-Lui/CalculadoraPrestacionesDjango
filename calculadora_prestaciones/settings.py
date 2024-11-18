185299847641795d89b89a2f33b09b57e10a7711
Ezzz-Lui <ogn.lui@gmail.com>
2024-11-17 19:42:03 -0600

    Add build script, update settings for static files, and configure Vercel deployment

--- calculadora_prestaciones/settings.py
+++ calculadora_prestaciones/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-rgry#$s7naj8^$vko@7u(rauo9=ed76hk$y1&*8ln@r38w_^^$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -117,13 +118,13 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # Directorios donde buscar archivos estáticos
 STATICFILES_DIRS = [
     BASE_DIR / "static",  # Agrega la carpeta 'static' en la raíz
 ]
 # Solo para producción (colecta estáticos en un solo lugar)
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
